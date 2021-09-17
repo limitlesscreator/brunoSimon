026eb8d394c5a172d050313f48352671582e9c2e
Vladislav V <0428vlad0428@mail.ru>
2021-09-17 21:01:59 +0300

    finished control camera)

--- 07/src/script.js
+++ 07/src/script.js
@@ -1,5 +1,18 @@
 import './style.css'
 import * as THREE from 'three'
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX  / sizes.width - 0.5
+    cursor.y = - (event.clientY  / sizes.height - 0.5)
+})
+
+console.log(OrbitControls)
 
 /**
  * Base
@@ -12,26 +25,41 @@ const sizes = {
     width: 800,
     height: 600
 }
-
 // Scene
 const scene = new THREE.Scene()
-
+// var loader = new THREE.TextureLoader();
 // Object
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    new THREE.MeshBasicMaterial({color: 0xff0000}),
+    // new THREE.MeshBasicMaterial({
+    //     map: loader.load('https://s3.amazonaws.com/duhaime/blog/tsne-webgl/assets/cat.jpg')
+    // })
 )
+
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75 , sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75 , sizes.width / sizes.height, 0.1, 100)
+// const aspectRation = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRation,
+//     1 * aspectRation,
+//     1,
+//     -1,
+//     0.1,
+//     100)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
-// Renderer 
+
+// Controls
+const controls = new OrbitControls(camera,  canvas)
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
@@ -40,12 +68,18 @@ renderer.setSize(sizes.width, sizes.height)
 // Animate
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+    // camera.position.x = cursor.x * 3
+    // camera.position.y = cursor.y * 3
+
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2 ) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
